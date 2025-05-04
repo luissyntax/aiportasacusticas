68032d72d9a8f0e6be5884ff0195890e9a5f5988
Luís Ribeiro <contato.ribeiroluis@gmail.com>
2025-05-04 12:10:20 -0300

    Alteração completa no site

--- js/header.js
+++ js/header.js
@@ -1,6 +1,13 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
+const body = document.body;
 
 hamburguer.addEventListener("click", () => {
-    nav.classList.toggle("active")
+    nav.classList.toggle("active");
+
+    if (nav.classList.contains("active")) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "";
+    }
 });
